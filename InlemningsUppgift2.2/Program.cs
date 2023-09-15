70316cf1352601cc30ac0e5c7e64e54984886acb
baronvonpurr <robinhood.svensson@hotmail.com>
2023-09-15 13:14:35 +0200

    Add project files.

---
+++ InlemningsUppgift2.2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace InlemningsUppgift2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur långt hoppade Elin?");
+            Double Elinhopp = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Hur långt hoppade Alma?");
+            Double Almahopp = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Elin hoppade" + (Elinhopp - Almahopp) + "längre än alma");
+        }
+    }
+}
